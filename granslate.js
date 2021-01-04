aaa56743b7ad34aa015a9ef8ef3dc66ce8a8728b
Husam Omar <=homar@asaltech.com>
2021-01-04 10:59:14 +0200

    format caode

--- granslate.js
+++ granslate.js
@@ -13,7 +13,7 @@ var body = document.getElementsByTagName("body")[0];
 var menu = document.createElement("menu");
 var subMenu = document.createElement("menu");
 var openNewTapMenu = document.createElement("menuItem");
-var menuItem=document.createElement("menuItem");
+var menuItem = document.createElement("menuItem");
 menu.setAttribute("type", "context");
 menu.setAttribute("id", "gtranslateMenu");
 body.appendChild(menu);
@@ -32,7 +32,7 @@ var selectedText = "";
 document.addEventListener("mouseup", (event) => {
   selectedText = getSelectedText();
 
-  if(selectedText != "" && event.button == 0){
+  if (selectedText != "" && event.button == 0) {
     var results = translateFun(selectedText);
     selectedText = "";
   }
@@ -40,16 +40,16 @@ document.addEventListener("mouseup", (event) => {
 });
 
 openNewTapMenu.addEventListener("click", (ev) => {
-	selectedText = getSelectedText();
-	if(ev.button == 0 && selectedText != ""){
-		var url = "https://translate.google.com/?sl=en&tl=ar&text=" + selectedText;
-		var win = window.open(url, '_blank');
-		win.focus();	
-	}
+  selectedText = getSelectedText();
+  if (ev.button == 0 && selectedText != "") {
+    var url = "https://translate.google.com/?sl=en&tl=ar&text=" + selectedText;
+    var win = window.open(url, '_blank');
+    win.focus();
+  }
 });
 
-    
-	  
+
+
 
 
 
@@ -58,43 +58,43 @@ body.addEventListener('contextmenu', (ev) => {
 
 
   let selectedTxt = getSelectedText();
-  subMenu.setAttribute("label", "translate '"+selectedTxt+"'");
+  subMenu.setAttribute("label", "translate '" + selectedTxt + "'");
 
   return false;
 }, false);
 
 
-async function translateFun(selectedText){
-  let results =  await translate(selectedText);
+async function translateFun(selectedText) {
+  let results = await translate(selectedText);
   menuItem.setAttribute("label", results);
   return results;
 }
 
 
 
-async function translate(text){
-   return new Promise(function(resolve, reject) {
-      var xhr = new XMLHttpRequest();
+async function translate(text) {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
 
-      xhr.open('GET', 'https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=ar&dt=t&q='+text, true);
-      xhr.setRequestHeader("Access-Control-Allow-Origin", "https://translate.googleapis.com");
-      xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.open('GET', 'https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=ar&dt=t&q=' + text, true);
+    xhr.setRequestHeader("Access-Control-Allow-Origin", "https://translate.googleapis.com");
+    xhr.setRequestHeader("Content-Type", "application/json");
 
-      xhr.onload = function() {
-        if (xhr.status == 200) {
-          // alert("results");
+    xhr.onload = function () {
+      if (xhr.status == 200) {
+        // alert("results");
 
-          resolve(JSON.parse(this.responseText)[0][0][0]);
-        }
-        else {
-          //alert(xhr.statusText);
+        resolve(JSON.parse(this.responseText)[0][0][0]);
+      }
+      else {
+        //alert(xhr.statusText);
 
-          reject(Error(xhr.statusText));
-        }
-      };
-      xhr.onerror = function() {
-        reject(Error("Network Error"));
-      };
-      xhr.send();
-    });
+        reject(Error(xhr.statusText));
+      }
+    };
+    xhr.onerror = function () {
+      reject(Error("Network Error"));
+    };
+    xhr.send();
+  });
 }
