b20cc714a941e1b9f36abb9262673221cf1f5288
Husam Omar <=homar@asaltech.com>
2021-01-18 00:07:18 +0200

    refactor

---
+++ src/background.js
@@ -0,0 +1,50 @@
+
+const granslateMenuId = "granslate-menu";
+
+browser.contextMenus.create({
+    id: granslateMenuId,
+    title: "translate ''",
+    contexts: ["all"],
+
+});
+
+browser.contextMenus.onShown.addListener(async function (info, tab) {
+    var selectedText = info.selectionText;
+    if (selectedText) {
+        let translation = await translate(selectedText);
+        browser.contextMenus.update(granslateMenuId, {
+            title: translation,
+            visible: true
+        });
+    } else {
+        browser.contextMenus.update(granslateMenuId, {
+            visible: false
+        });
+    }
+    browser.contextMenus.refresh();
+
+});
+
+
+async function translate(text) {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=ar&dt=t&q=' + text, true);
+        xhr.setRequestHeader("Access-Control-Allow-Origin", "https://translate.googleapis.com");
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.onload = function () {
+            if (xhr.status == 200) {
+                resolve(JSON.parse(this.responseText)[0][0][0]);
+            }
+            else {
+                reject(Error(xhr.statusText));
+            }
+        };
+        xhr.onerror = function () {
+            reject(Error("Network Error"));
+        };
+        xhr.send();
+    });
+}
